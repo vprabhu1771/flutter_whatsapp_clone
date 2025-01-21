44c0c0088bb2c604310b365337fb8ac76fba1c95
vprabhu1771 <vprabhu1771@gmail.com>
2025-01-22 02:25:58 +0530

    PrivacyAndTerms widget added to Welcome Screen

--- lib/screens/WelcomeScreen.dart
+++ lib/screens/WelcomeScreen.dart
@@ -38,21 +38,7 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                       'Welcome to Whatsapp',
                     style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold, color: Colors.white),
                   ),
-                  Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
-                      child: RichText(
-                          textAlign: TextAlign.center,
-                          text: TextSpan(
-                              text: 'Read our ',
-                              style: TextStyle(color: ColorConstants.GRAY, height: 1.5,),
-                              children: [
-                                TextSpan(text: 'Privacy Policy ', style: TextStyle(color: ColorConstants.SKY_BLUE)),
-                                TextSpan(text: 'Tap "Agree and continue" to accept the '),
-                                TextSpan(text: 'Terms of Services.', style: TextStyle(color: ColorConstants.SKY_BLUE)),
-                              ]
-                          )
-                      )
-                  ),
+                  PrivacyAndTerms(),
                   SizedBox(
                     height: 42,
                     width: MediaQuery.of(context).size.width - 100,
