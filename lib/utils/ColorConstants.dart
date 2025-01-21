cc80103e2f79fecd63915762e295673cb6bc1bb4
vprabhu1771 <vprabhu1771@gmail.com>
2025-01-22 02:43:38 +0530

    Color names updated

--- lib/utils/ColorConstants.dart
+++ lib/utils/ColorConstants.dart
@@ -4,15 +4,17 @@ class ColorConstants {
 
   static const Color BLACK = Color(0xFF111B21);
 
-  static const Color WHITE = Color(0xFF111B21);
+  static const Color GREEN_DARK = Color(0xFF00A884);
+  static const Color GREEN_LIGHT = Color(0xFF008069);
 
-  static const Color GREEN = Color(0xFF00A884);
+  static const Color BLUE_DARK = Color(0xFF53BDEB);
+  static const Color BLUE_LIGHT = Color(0xFF027EB5);
 
-  static const Color GRAY = Color(0xFF8696A0);
+  static const Color GRAY_DARK = Color(0xFF8696A0);
+  static const Color GRAY_LIGHT = Color(0xFF667781);
 
-  static const Color SKY_BLUE = Color(0xFF53BDEB);
-
-  static const Color CHARCOAL_BLACK = Color(0xFF111B21);
+  static const Color BACKGROUND_DARK = Color(0xFF111B21);
+  static const Color BACKGROUND_LIGHT = Color(0xFFFFFFFF);
 
   static const Color DEEP_BLUE_GRAY = Color(0xFF182229);
 
