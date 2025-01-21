af6b1d39f4ec1059492e4d845585611247c175d2
vprabhu1771 <vprabhu1771@gmail.com>
2025-01-21 23:30:33 +0530

    WelcomeScreen ui added

--- lib/screens/WelcomeScreen.dart
+++ lib/screens/WelcomeScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_whatsapp_clone/utils/ColorConstants.dart';
 
 class WelcomeScreen extends StatefulWidget {
 
@@ -12,9 +13,98 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Welcome to Whatsapp'),
-      ),
+      backgroundColor: ColorConstants.BLACK,
+      body: Column(
+        children: [
+          Expanded(
+              child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 50),
+                  child: Image.asset(
+                    'assets/images/circle.png',
+                    color: ColorConstants.GREEN,
+                  )
+              ),
+          ),
+          SizedBox(height: 60,),
+          Expanded(
+              child: Column(
+                children: [
+                  Text(
+                      'Welcome to Whatsapp',
+                    style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold, color: Colors.white),
+                  ),
+                  Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+                      child: RichText(
+                          textAlign: TextAlign.center,
+                          text: TextSpan(
+                              text: 'Read our ',
+                              style: TextStyle(color: ColorConstants.GRAY, height: 1.5,),
+                              children: [
+                                TextSpan(text: 'Privacy Policy ', style: TextStyle(color: ColorConstants.SKY_BLUE)),
+                                TextSpan(text: 'Tap "Agree and continue" to accept the '),
+                                TextSpan(text: 'Terms of Services.', style: TextStyle(color: ColorConstants.SKY_BLUE)),
+                              ]
+                          )
+                      )
+                  ),
+                  SizedBox(
+                    height: 42,
+                    width: MediaQuery.of(context).size.width - 100,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: ColorConstants.GREEN,
+                        foregroundColor: ColorConstants.CHARCOAL_BLACK,
+                        splashFactory: NoSplash.splashFactory,
+                        elevation: 0,
+                        shadowColor: Colors.transparent,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.zero, // Removes rounded corners
+                        ),
+                      ),
+                      child: Text("AGREE AND CONTINUE"),
+                    ),
+                  ),
+                  SizedBox(height: 50,),
+                  Material(
+                    color: ColorConstants.DEEP_BLUE_GRAY,
+                    borderRadius: BorderRadius.circular(20),
+                    child: InkWell(
+                      onTap: () {},
+                      borderRadius: BorderRadius.circular(20),
+                      splashFactory: NoSplash.splashFactory,
+                      highlightColor: ColorConstants.DARK_CYAN_BLACK,
+                      child: Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                          child: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Icon(
+                              Icons.language,
+                              color: ColorConstants.GREEN,
+                            ),
+                            SizedBox(width: 10,),
+                            Text(
+                              'English',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            SizedBox(width: 10,),
+                            Icon(
+                              Icons.keyboard_arrow_down,
+                              color: ColorConstants.GREEN,
+                            ),
+                          ],
+                        ),
+                      )
+                    ),
+                  )
+                ],
+              )
+          ),
+
+        ],
+      )
     );
   }
 }
