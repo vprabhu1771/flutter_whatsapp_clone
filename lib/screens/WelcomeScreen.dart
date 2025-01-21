68c5c6a727eced8bfc96b3350d2113e78cd35bd3
vprabhu1771 <vprabhu1771@gmail.com>
2025-01-22 02:36:30 +0530

    LanguageButton widget added to Welcome Screen

--- lib/screens/WelcomeScreen.dart
+++ lib/screens/WelcomeScreen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_whatsapp_clone/utils/ColorConstants.dart';
 import 'package:flutter_whatsapp_clone/widgets/CustomElevatedButton.dart';
+import 'package:flutter_whatsapp_clone/widgets/LanguageButton.dart';
 
 import '../widgets/PrivacyAndTerms.dart';
 
@@ -42,38 +43,7 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                   PrivacyAndTerms(),
                   CustomElevatedButton(onPressed: (){}, title: "AGREE AND CONTINUE", ),
                   SizedBox(height: 50,),
-                  Material(
-                    color: ColorConstants.DEEP_BLUE_GRAY,
-                    borderRadius: BorderRadius.circular(20),
-                    child: InkWell(
-                      onTap: () {},
-                      borderRadius: BorderRadius.circular(20),
-                      splashFactory: NoSplash.splashFactory,
-                      highlightColor: ColorConstants.DARK_CYAN_BLACK,
-                      child: Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                          child: Row(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            Icon(
-                              Icons.language,
-                              color: ColorConstants.GREEN,
-                            ),
-                            SizedBox(width: 10,),
-                            Text(
-                              'English',
-                              style: TextStyle(color: Colors.white),
-                            ),
-                            SizedBox(width: 10,),
-                            Icon(
-                              Icons.keyboard_arrow_down,
-                              color: ColorConstants.GREEN,
-                            ),
-                          ],
-                        ),
-                      )
-                    ),
-                  )
+                  LanguageButton()
                 ],
               )
           ),
