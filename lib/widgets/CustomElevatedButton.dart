2537d4b293e0e4b107e833f3370f6a249c91d940
vprabhu1771 <vprabhu1771@gmail.com>
2025-01-22 02:32:11 +0530

    Create CustomElevatedButton.dart

---
+++ lib/widgets/CustomElevatedButton.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+import '../utils/ColorConstants.dart';
+
+class CustomElevatedButton extends StatelessWidget {
+
+  final double? buttonWidth;
+
+  final VoidCallback onPressed;
+
+  final String title;
+
+  const CustomElevatedButton({
+    super.key,
+    this.buttonWidth,
+    required this.onPressed,
+    required this.title
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 42,
+      width: buttonWidth ?? MediaQuery.of(context).size.width - 100,
+      child: ElevatedButton(
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          backgroundColor: ColorConstants.GREEN,
+          foregroundColor: ColorConstants.CHARCOAL_BLACK,
+          splashFactory: NoSplash.splashFactory,
+          elevation: 0,
+          shadowColor: Colors.transparent,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.zero, // Removes rounded corners
+          ),
+        ),
+        child: Text(this.title),
+      ),
+    );
+  }
+}
