c4f1e2d8ab143f75a710cb32849ca06115c46977
vprabhu1771 <vprabhu1771@gmail.com>
2025-01-21 23:30:08 +0530

    Create ColorConstants.dart

---
+++ lib/utils/ColorConstants.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class ColorConstants {
+
+  static const Color BLACK = Color(0xFF111B21);
+
+  static const Color WHITE = Color(0xFF111B21);
+
+  static const Color GREEN = Color(0xFF00A884);
+
+  static const Color GRAY = Color(0xFF8696A0);
+
+  static const Color SKY_BLUE = Color(0xFF53BDEB);
+
+  static const Color CHARCOAL_BLACK = Color(0xFF111B21);
+
+  static const Color DEEP_BLUE_GRAY = Color(0xFF182229);
+
+  static const Color DARK_CYAN_BLACK = Color(0xFF09141A);
+
+}
